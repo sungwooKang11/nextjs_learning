de4bbe0d323d00a738be067293793b7cbf20c860
sungwookang <sungwookang11@gmail.com>
2022-11-03 02:22:46 +0900

    22.11.03 : catch-all url, 404 page

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,9 @@ export default function Home({results}) {
 
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
+    router.push(`/movies/${title}/${id}`,
+
+
     );
   };
 
@@ -30,13 +25,7 @@ export default function Home({results}) {
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
             <h4>
               <Link
-                href={{
-                  pathname: `/movies/${movie.id}`,
-                  query: {
-                    title: movie.original_title,
-                  },
-                }}
-                as={`/movies/${movie.id}`}
+                href={`/movies/${movie.original_title}${movie.id}`}
               >
                 <a>{movie.original_title}</a>
               </Link>
